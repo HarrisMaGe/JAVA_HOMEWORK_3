9b9fd67a85e427f598b6b1bf854ad9532b720fa1
马永成 <2015302580305@whu.edu.cn>
2016-11-19 23:54:15 +0800

    finally

--- src/Matching/GetFingerprint.java
+++ src/Matching/GetFingerprint.java
@@ -1,4 +1,4 @@
-package Matching;
+﻿package Matching;
 
 import Database.OpenDB;
 import Fingerprint.Fingerprint;
@@ -10,7 +10,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 /**
  * Created by 冬至节 on 2016/11/12.
@@ -27,7 +26,8 @@ public class GetFingerprint {
     static ResultSet rs = null;
     static String url = "jdbc:mysql://127.0.0.1:330/homework_3?useSSL=false";
 
-    public int[][] Hashes(String path) throws IOException{
+    // 读取待测文件信息
+    public int[][] Hashs(String path) throws IOException{
         fp.divide(read.getDoubles(path));
         read.deleteArray();
         List finger_id = new ArrayList();
@@ -52,10 +52,26 @@ public class GetFingerprint {
             openDB = new OpenDB(url);
             for (int i = 0; i < this.h.length; i++) {
                 String sql = "select song_id,offset from songfinger where finger_id = " + String.valueOf(this.h[i][0]);
-                PreparedStatement pStmt = openDB.conn.prepareStatement(sql);
+                PreparedStatement pStmt = openDB.conn.prepareStatement(sql,ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
                 ResultSet rs = pStmt.executeQuery(sql);
-                while (rs.next()) {
-                    openDB.insertToMatchsong(rs.getInt("song_id"),rs.getInt("offset")-this.h[i][1]);
+                rs.last();
+                int count = rs.getRow();
+                rs.beforeFirst();
+
+                String insql = "insert into matchsong(song_id,dif_offset)values(?,?)";
+                PreparedStatement pStmt_1 = openDB.conn.prepareStatement(insql,ResultSet.TYPE_SCROLL_SENSITIVE);
+
+               // pStmt.executeUpdate();
+
+                for(int j = 0;j< count;j++) {
+                    for(int k = 0;k<1000;k++){
+                        if(rs.next()) {
+                            pStmt_1.setInt(1, rs.getInt("song_id"));
+                            pStmt_1.setInt(2, rs.getInt("offset") - this.h[i][1]);
+                            pStmt_1.addBatch();
+                        }
+                    }
+                    pStmt_1.executeBatch();
                 }
                 rs.close();
             }
@@ -65,20 +81,30 @@ public class GetFingerprint {
             while (resultSet.next()) {
                 int selectId = resultSet.getInt(1);
                 int score = 0;
-                Random random = new Random();
                 String selsql = "select count(*) from (select dif_offset from matchsong where song_id = "
                         +String.valueOf(selectId)+") as num group by dif_offset";
+                List score_times = new ArrayList();
                 PreparedStatement pStmt_1 = openDB.conn.prepareStatement(selsql);
                 ResultSet rs_1 = pStmt_1.executeQuery(selsql);
                 while (rs_1.next()) {
-                   if(rs_1.getInt(1)>8){
-                        score = score+9 + random.nextInt(10);
+
+                    score_times.add(rs_1.getInt(1));
+                   //if(rs_1.getInt(1)>8){
+                   //     score = score+9;//寻找峰值，每有一个较高的峰加9分
+                   // }
+                }
+                //找到最高峰值作为打分
+                int max_score = (int)score_times.get(0);
+                for(int i =1;i<score_times.size();i++){
+                    if((int)score_times.get(i)>max_score){
+                        max_score = (int)score_times.get(i);
                     }
                 }
+                score = max_score;
                 int [] id_score = new int[2];
                 id_score[0] = selectId;
                 id_score[1] = score;
-               matchArray.add(id_score);
+               matchArray.add(id_score);//数组第一位存储歌曲Id,第二位存储分数
              }
              openDB.close();
             return matchArray;
